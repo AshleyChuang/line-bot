46d0ddb0a9ee0146cd4a0ae9ebe329f2729d969a
ashley <ashleyashley19951213@gmail.com>
2018-04-21 00:24:33 +0800

    test

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
+    line_bot_api.reply_message(event.reply_token,"hahahahahaha")
 
 import os
 if __name__ == "__main__":
