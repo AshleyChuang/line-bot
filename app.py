0da5473f5fa259846f424fbd57ef24e8f8155c26
ashley <ashleyashley19951213@gmail.com>
2018-04-21 17:16:28 +0800

    button template add image

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def handle_message(event):
     buttons_template = ButtonsTemplate(
         type='buttons', title=movie_name,
         text='Please select!',
+        thumbnailImageUrl = movie_pic,
         actions=[URITemplateAction(type = 'uri',label='Link to Viewshow', uri=movie_url)]
         )
     message = TemplateSendMessage(
