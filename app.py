dfe8e39510768de98fe502b41354a3d53d030477
ashley <ashleyashley19951213@gmail.com>
2018-04-21 17:30:11 +0800

    button template image size

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def handle_message(event):
     buttons_template = ButtonsTemplate(
         type='buttons', title=movie_name,
         text='Please select!',
-        thumbnail_image_url = movie_pic, imageSize = 'contain',
+        thumbnail_image_url = movie_pic, imageSize = 'contain', image_aspect_ratio = 'square',
         actions=[URITemplateAction(type = 'uri',label='Link to Viewshow', uri=movie_url)]
         )
     message = TemplateSendMessage(
