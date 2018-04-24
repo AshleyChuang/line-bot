7ab54b9890e1881b73494ecab8073629e05c2148
ashley <ashleyashley19951213@gmail.com>
2018-04-24 14:47:11 +0800

    get userid

--- app.py
+++ app.py
@@ -210,24 +210,9 @@ def get_hot_movie_list():
     message = TemplateSendMessage(type='template', alt_text='Hot Movie List', template=imagecarousel)
     return [TextSendMessage(text='這是本週熱門電影喔！可以參考看看～'),message]
 
-@app.route('/')
-def greeting():
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    #app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return "Hello Bot User!"
-
 @handler.add(FollowEvent):
 def handle_follow(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Hello! "+event.source.userId))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Hello! "))
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
